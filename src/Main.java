5b856e3651a78b87b04c87ab761e41df2229b920
Neto test <zilya.zakirova1991@gmail.com>
2023-03-23 14:39:46 +0300

    new MpbileProject

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int started = 200;
+        int add = 1200;
+        int bonus;
+
+        if (add >= 1000) {
+            bonus =add / 100;
+           // "суммарный бонус=bonus";
+        } else {
+           // "суммарный бонус=0";
+            bonus=0;
+
+        }
+        int sum= started+add+bonus;
+        System.out.println(bonus);
+        System.out.println(sum);
     }
 }
